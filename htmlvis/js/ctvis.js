fc06e71b22660375700c62ce5ff1a1bbb2821335
Emzo de los Santos <emzodls@gmail.com>
2017-08-23 17:44:42 +0100

    added divs to html file

--- htmlvis/js/ctvis.js
+++ htmlvis/js/ctvis.js
@@ -8,32 +8,39 @@ function ct(ct_data, ct_scale, hits,hit_colors,hmms,hmm_colors,container) {
     
   $("#" + container).html("");
   var ui = $("#" + container);
-    ui.append("<h3>ClusterTools Hits</h3>")
+    ui.append('<div id="ctHits">')
+    var ctHits = $("#ctHits");
+    ctHits.append("<h3>ClusterTools Hits</h3>")
   for (var i in ct_data) {
-  	ui.append("<h4>" + ct_data[i]["id"] + " (Location: " + ct_data[i]["start"] + "-" + ct_data[i]["end"] + ", Size: " + ct_data[i]["size"] + " nts)" + "<h4>");
+    ctHits.append('<div id="hit-' + i + '">')
+    var hitDiv = $("#hit-"+i);
+    
+  	hitDiv.append("<h4>" + ct_data[i]["id"] + " (Location: " + ct_data[i]["start"] + "-" + ct_data[i]["end"] + ", Size: " + ct_data[i]["size"] + " nts)" + "</h4>");
     var svg = $(Arrower.drawClusterToolsSVG(ct_data[i],ct_scale));
     svg.css("clear", "both");
     svg.addClass("arrower-svg");
-    ui.append(svg);
-    ui.find("svg.arrower-svg").each(
+    hitDiv.append(svg);
+    hitDiv.find("svg.arrower-svg").each(
     	function(){
     		$(this).attr("width",$(this).find("g")[0].getBoundingClientRect().width);
     		$(this).attr("height",$(this).find("g")[0].getBoundingClientRect().height*1.3);
     	});
   }
-    ui.append("<h3>Legend</h3>");
-    ui.append("<h4>Queries<h4>");
-    ui.append("<div>")
+    ui.append('<div id="legend">')
+    var legendDiv = $("#legend");
+    legendDiv.append("<h3>Legend</h3>");
+    legendDiv.append("<h4>Queries</h4>");
+    legendDiv.append('<div id="geneQueries">');
+    var geneQueries = $("#geneQueries");
     for (var i in hits){
-        ui.append('<svg width="20px" height="20px"><rect width="20px" height="20px" style="fill:' + hit_colors[i]+ ';stroke-width:1;stroke:rgb(0,0,0)"/></svg>')
-        ui.append('\t'+hits[i]+'\t')
+        geneQueries.append('<svg width="20px" height="20px"><rect width="20px" height="20px" style="fill:' + hit_colors[i]+ ';stroke-width:1;stroke:rgb(0,0,0)"/></svg>');
+        geneQueries.append('\t'+hits[i]+'\t');
     }
-    ui.append("</div>")
-    ui.append("<h4>HMMs<h4>");
-    ui.append("<div>")
+    legendDiv.append("<h4>HMMs</h4>");
+    legendDiv.append('<div id="hmmQueries">');
+    var hmmQueries = $("#hmmQueries");
     for (var i in hmms){
-        ui.append('<svg width="20px" height="20px"><rect width="20px" height="20px" style="fill:' + hmm_colors[i]+ ';stroke-width:1;stroke:rgb(0,0,0)"/></svg>')
-        ui.append('\t'+hmms[i]+'\t')
+        hmmQueries.append('<svg width="20px" height="20px"><rect width="20px" height="20px" style="fill:' + hmm_colors[i]+ ';stroke-width:1;stroke:rgb(0,0,0)"/></svg>');
+        hmmQueries.append('\t'+hmms[i]+'\t');
     }
-    ui.append("</div>")
 }
